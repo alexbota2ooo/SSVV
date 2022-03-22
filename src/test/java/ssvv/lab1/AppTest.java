08b5ae79d831e5d9abfd03e03369e2cee3b13148
cborodi <cristi_borodi@yahoo.com>
2022-03-22 12:37:54 +0200

    10 Tests

--- src/test/java/ssvv/lab1/AppTest.java
+++ src/test/java/ssvv/lab1/AppTest.java
@@ -1,8 +1,5 @@
 package ssvv.lab1;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import domain.Student;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -14,6 +11,10 @@ import service.Service;
 import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
+import validation.ValidationException;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit test for simple App.
@@ -58,4 +59,99 @@ public class AppTest
         Student student = new Student("999", "boro", 931, "boro@yahoo.com");
         assertNull(service.addStudent(student));
     }
+
+
+    @Test
+    public void testAddStudent2() {
+        // Duplicate
+        Student student = new Student("999", "boro", 931, "boro@yahoo.com");
+        assertNotNull(service.addStudent(student));
+    }
+
+    @Test
+    public void testAddStudent3() {
+        // Validation Error
+        Student student = new Student("", "boro", 931, "boro@yahoo.com");
+        Exception exception = assertThrows(ValidationException.class, ()->{service.addStudent(student);});
+
+        String expectedMessage = "Id incorect!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testAddStudent4() {
+        // Validation Error
+        Student student = new Student("999", "", 931, "boro@yahoo.com");
+        Exception exception = assertThrows(ValidationException.class, ()->{service.addStudent(student);});
+
+        String expectedMessage = "Nume incorect!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testAddStudent5() {
+        // Validation Error
+        Student student = new Student("999", "boro", -1, "boro@yahoo.com");
+        Exception exception = assertThrows(ValidationException.class, ()->{service.addStudent(student);});
+
+        String expectedMessage = "Grupa incorecta!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testAddStudent6() {
+        // Validation Error
+        Student student = new Student("999", "boro", 931, null);
+        Exception exception = assertThrows(ValidationException.class, ()->{service.addStudent(student);});
+
+        String expectedMessage = "Email incorect!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testAddStudent7() {
+        // Validation Error
+        Student student = new Student("999", null, 931, "boro@yahoo.com");
+        Exception exception = assertThrows(ValidationException.class, ()->{service.addStudent(student);});
+
+        String expectedMessage = "Nume incorect!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testAddStudent8() {
+        // Validation Error
+        Student student = new Student("999", "boro", 931, "");
+        Exception exception = assertThrows(ValidationException.class, ()->{service.addStudent(student);});
+
+        String expectedMessage = "Email incorect!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testAddStudent9() {
+        // BVA
+        Student student = new Student("998", "boro", 0, "boro@yahoo.com");
+        assertNull(service.addStudent(student));
+    }
+
+    @Test
+    public void testAddStudent10() {
+        // BVA
+        Student student = new Student("997", "boro", 1, "boro@yahoo.com");
+        assertNull(service.addStudent(student));
+    }
+
 }
